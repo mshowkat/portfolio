8ef0d4ca7f0b8b0aa83d6f2764f30e5051e1b546
mshowkat <bangleblaze@gmail.com>
2019-08-22 22:55:11 +0600

    Updates

---
+++ precache-manifest.ff0c732bd93b2c31bc94ae969005d80d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9613ef4f25c84cade8b00e469aca3500",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4fa58f06ad0717da14b8",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "b2dddd0cb2ef8311da08",
+    "url": "/portfolio/static/css/main.6e131a0c.chunk.css"
+  },
+  {
+    "revision": "4fa58f06ad0717da14b8",
+    "url": "/portfolio/static/js/2.5dbf3159.chunk.js"
+  },
+  {
+    "revision": "b2dddd0cb2ef8311da08",
+    "url": "/portfolio/static/js/main.a532fdcb.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "fe67ee6cc7b9aae77c7330ee941d3ea6",
+    "url": "/portfolio/static/media/000.fe67ee6c.jpg"
+  },
+  {
+    "revision": "577362e68ce6e03054a63d0803393c9f",
+    "url": "/portfolio/static/media/01.577362e6.png"
+  },
+  {
+    "revision": "55d9657a17482772b3eb6cb580cd902c",
+    "url": "/portfolio/static/media/color.55d9657a.png"
+  },
+  {
+    "revision": "b1265dbefd192723fb1ba3c61247e475",
+    "url": "/portfolio/static/media/jsDoc.b1265dbe.png"
+  },
+  {
+    "revision": "362114efbb098872e4750233b5186d05",
+    "url": "/portfolio/static/media/rest.362114ef.png"
+  },
+  {
+    "revision": "b6df86e9f0b6bc1edac464016d9628d4",
+    "url": "/portfolio/static/media/todo.b6df86e9.png"
+  }
+]);
